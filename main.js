4d96483874e83de35f90ab92844824a8420b9e71
PavelKisielski <starykasztannarowerze@gmail.com>
2026-01-15 14:33:54 +0100

    moj pierwszy commit

---
+++ main.js
@@ -0,0 +1,3 @@
+  document.getElementById("changeBgBtn").addEventListener("click", () => {
+    document.body.style.backgroundImage = `url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.ceneostatic.pl%2Fdata%2Fproducts%2F23195555%2Fi-buddy-toys-samochody-budowlane-betoniarka.jpg&f=1&nofb=1&ipt=65aa991124496a5ca0ba17adee67298fa78f9967b551e15561b14715b167da6b')`;
+  });
